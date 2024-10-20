accc4fe257191b76623f8ef8684007915af98f01
c0mplex <snakebigbosslel@gmail.com>
2024-10-20 23:23:15 +0800

    Renamed Home to Login

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -4,16 +4,16 @@ using System.Diagnostics;
 
 namespace MSMS.Controllers;
 
-public class HomeController : Controller
+public class LoginController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
+    private readonly ILogger<LoginController> _logger;
 
-    public HomeController(ILogger<HomeController> logger)
+    public LoginController(ILogger<LoginController> logger)
     {
         _logger = logger;
     }
 
-    public IActionResult Index()
+    public IActionResult Login()
     {
         return View();
     }
