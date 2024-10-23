477d8e3c6c893c99129656837ddfe3afddad34f8
c0mplex <snakebigbosslel@gmail.com>
2024-10-23 23:33:33 +0800

    user now in Models.Dashboard, and created Dashboard for main site development WIP

---
+++ Models/Dashboard/User.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MSMS.Models.Dashboard;
+
+public class User
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+
+    [Required]
+    [StringLength(20, MinimumLength = 7)]
+    public string Username { get; set; } = string.Empty;
+
+    [Required]
+    [DataType(DataType.Password)]
+    [StringLength(50, MinimumLength = 7)]
+    public string Password { get; set; } = string.Empty;
+
+    public string Role { get; set; } = string.Empty;
+
+}
