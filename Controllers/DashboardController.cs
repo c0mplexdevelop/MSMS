2274f0b9875046bf67f75ab669c995a65838ab64
c0mplex <snakebigbosslel@gmail.com>
2024-10-23 23:33:44 +0800

    Forgor to commit the controller

---
+++ Controllers/DashboardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MSMS.Models.Dashboard;
+
+namespace MSMS.Controllers;
+
+public class DashboardController : Controller
+{
+    public IActionResult Index(User user)
+    {
+        return View(user);
+    }
+}
