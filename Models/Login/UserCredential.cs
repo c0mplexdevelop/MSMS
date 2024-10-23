2a99e4760b0897a439582b5c7d9ba85713fb6c9b
c0mplex <snakebigbosslel@gmail.com>
2024-10-23 23:34:49 +0800

    Updated to UserCredential for XRef to User in DB

---
+++ Models/Login/UserCredential.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MSMS.Models.Login;
+
+public class UserCredential
+{
+    [Required]
+    [StringLength(20, MinimumLength = 7, ErrorMessage = "Username must be between 7 and 20 characters long")]
+    public string Username { get; set; } = string.Empty;
+
+    [Required]
+    [DataType(DataType.Password)]
+    [StringLength(int.MaxValue, MinimumLength = 7, ErrorMessage = "Password must be at least 7 characters long")]
+    public string Password { get; set; } = string.Empty;
+}
