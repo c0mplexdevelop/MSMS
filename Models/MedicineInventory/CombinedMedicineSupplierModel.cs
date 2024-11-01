7da0f335d59eacde35a6cecb63e2ba08b5cec862
c0mplex <snakebigbosslel@gmail.com>
2024-11-01 14:28:57 +0800

    Changed to model to use ViewModel instead

--- Models/MedicineInventory/CombinedMedicineSupplierModel.cs
+++ Models/MedicineInventory/CombinedMedicineSupplierModel.cs
@@ -1,7 +1,10 @@
-﻿namespace MSMS.Models.MedicineInventory;
+﻿using MSMS.Models.Dashboard;
+
+namespace MSMS.Models.MedicineInventory;
 
 public class CombinedMedicineSupplierModel
 {
     public IEnumerable<Medicine> Medicines { get; set; } = null!;
     public IEnumerable<Supplier> Suppliers { get; set; } = null!;
+    public IEnumerable<User> User { get; set; } = null!;
 }
