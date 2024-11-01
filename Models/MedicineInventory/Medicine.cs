a3718ac103ce9dda3668f2d5d3bdd79c8fbf8aac
c0mplex <snakebigbosslel@gmail.com>
2024-11-01 14:31:13 +0800

    Updated Model to follow high-fidelity

--- Models/MedicineInventory/Medicine.cs
+++ Models/MedicineInventory/Medicine.cs
@@ -1,19 +1,27 @@
-﻿using System.ComponentModel.DataAnnotations;
-
+﻿
 namespace MSMS.Models.MedicineInventory;
 
 public class Medicine
 {
     public int Id { get; set; }
-    [Required]
     public string BrandName { get; set; } = string.Empty;
-    [Required]
     public string GenericName { get; set; } = string.Empty;
-    public string? Description { get; set; }
+    public int Quantity { get; set; }
+    public int Dosage { get; set; }
     public DateOnly ExpiryDate { get; set; }
-    public int ActualQuantity { get; set; }
-    public int RecommendedQuantity { get; set; }
-    public int ReorderLevel { get; set; } // This field will be used to determine when to reorder the medicine
+    public DateOnly SuppliedDate { get; set; }
     public decimal Price { get; set; }
+
+    public int SupplierId { get; set; }
     public Supplier Supplier { get; set; } = null!;
+    /*
+     * If Quantity is less than ReorderLevel, then reorder the medicine
+     */
+    public int ReorderLevel { get; set; } // This field will be used to determine when to reorder the medicine
+
+    public override string ToString()
+    {
+        return $"Id: {Id}, BrandName: {BrandName}, GenericName: {GenericName}, Quantity: {Quantity}, Dosage: {Dosage}, ExpiryDate: {ExpiryDate}, SuppliedDate: {SuppliedDate}, Price: {Price}, Supplier: {Supplier}, ReorderLevel: {ReorderLevel}";
+    }
+
 }
