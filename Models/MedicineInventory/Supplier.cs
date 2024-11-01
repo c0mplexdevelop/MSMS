6a8a0de663b14f0eccad70aa17a43b004dbeda6b
c0mplex <snakebigbosslel@gmail.com>
2024-11-01 14:31:35 +0800

    Provided a many-to-one relationship to medicine

--- Models/MedicineInventory/Supplier.cs
+++ Models/MedicineInventory/Supplier.cs
@@ -1,15 +1,12 @@
-﻿using System.ComponentModel.DataAnnotations;
-
+﻿
 namespace MSMS.Models.MedicineInventory;
 
 public class Supplier
 {
-    [Required]
     public int Id { get; set; }
-    [Required]
     public string Name { get; set; } = string.Empty;
-    [Required]
     public string Address { get; set; } = string.Empty;
-    [Required]
     public string ContactNumber { get; set; } = string.Empty;
+
+    public ICollection<Medicine> Medicines { get; set; } = null!;
 }
