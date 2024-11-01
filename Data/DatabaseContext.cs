0b383ebd25c78948938eea664456f9fa9b1abb78
c0mplex <snakebigbosslel@gmail.com>
2024-11-01 14:30:41 +0800

    FOR TESTING: seeded data and added Suppliers table

--- Data/DatabaseContext.cs
+++ Data/DatabaseContext.cs
@@ -9,11 +9,57 @@ public class DatabaseContext : DbContext
 {
 
     public DbSet<Medicine> Medicines { get; set; }
+    public DbSet<Supplier> Suppliers { get; set; }
     public DbSet<User> Users { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseInMemoryDatabase("MSMS");
+        optionsBuilder.EnableSensitiveDataLogging();
         base.OnConfiguring(optionsBuilder);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Medicine>().HasOne(m => m.Supplier).WithMany(supplier => supplier.Medicines).HasForeignKey(m => m.SupplierId);
+
+        modelBuilder.Entity<Supplier>().HasData(
+            new Supplier
+            {
+                Id = 1,
+                Name = "Supplier 1",
+                Address = "Address 1",
+                ContactNumber = "1234567890"
+            },
+            new Supplier
+            {
+                Id = 2,
+                Name = "Supplier 2",
+                Address = "Address 2",
+                ContactNumber = "0987654321"
+            }
+        );
+
+        modelBuilder.Entity<Medicine>().HasData(
+            new Medicine
+            {
+                Id = 1,
+                GenericName = "Paracetamol",
+                Price = 10.00m,
+                Quantity = 100,
+                SupplierId = 1
+            },
+
+            new Medicine
+            {
+                Id = 2,
+                GenericName = "Aspirin",
+                Price = 5.00m,
+                Quantity = 50,
+                SupplierId = 2
+            }
+        );
+
+        base.OnModelCreating(modelBuilder);
     }
 }
