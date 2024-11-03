0edd28878bfaa19ea8e89ee913c7ab6320f895c1
c0mplex <snakebigbosslel@gmail.com>
2024-11-03 18:24:00 +0800

    Removed unnecessary seeding (wrong place to do so)

--- Data/Repos/MedicineRepository.cs
+++ Data/Repos/MedicineRepository.cs
@@ -11,25 +11,46 @@ public class MedicineRepository : IMedicineDatabaseRepository
         {
         this.context = context;
         }
+    //public MedicineRepository()
+    //{
+        //var supplier1 = new Supplier
+        //{
+        //    Id = 1,
+        //    Name = "Supplier 1",
+        //    Address = "Address 1",
+        //    ContactNumber = "1234567890"
+        //};
+        //var supplier2 = new Supplier
+        //{
+        //    Id = 2,
+        //    Name = "Supplier 2",
+        //    Address = "Address 2",
+        //    ContactNumber = "0987654321"
+        //};
 
-        var medicine1 = new Medicine
-        {
-            Id = 1,
-            GenericName = "Paracetamol",
-            Price = 10.00m,
-            Quantity = 100,
-            Supplier = supplier1
-        };
+        //if(!context.Suppliers.Any())
+        //{
+        //    context.Suppliers.AddRange(supplier1, supplier2);
+        //    context.SaveChanges();
+        //}
 
-        var medicine2 = new Medicine
-        {
-            Id = 2,
-            GenericName = "Aspirin",
-            Price = 5.00m,
-            Quantity = 50,
-            Supplier = supplier2
-        };
+        //var medicine1 = new Medicine
+        //{
+        //    Id = 1,
+        //    GenericName = "Paracetamol",
+        //    Price = 10.00m,
+        //    Quantity = 100,
+        //    Supplier = supplier1
+        //};
 
+        //var medicine2 = new Medicine
+        //{
+        //    Id = 2,
+        //    GenericName = "Aspirin",
+        //    Price = 5.00m,
+        //    Quantity = 50,
+        //    Supplier = supplier2
+        //};
         
         if(!context.Medicines.Any())
         {
