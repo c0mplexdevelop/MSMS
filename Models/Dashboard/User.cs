3671e2ca9d1dca0d583683e28a1e6c8e53a8afe2
c0mplex <snakebigbosslel@gmail.com>
2024-11-11 18:35:14 +0800

    .

--- Models/Dashboard/User.cs
+++ Models/Dashboard/User.cs
@@ -1,5 +1,5 @@
 ﻿using MSMS.Auth;
-﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
 
 namespace MSMS.Models.Dashboard;
 
@@ -11,15 +11,13 @@ public class User
     public string Email { get; set; } = string.Empty;
 
     [Required]
-    [StringLength(20, MinimumLength = 7)]
     public string Username { get; set; } = string.Empty;
 
     [Required]
     [DataType(DataType.Password)]
-    [StringLength(50, MinimumLength = 7)]
     public string Password { get; set; } = string.Empty;
 
-    public string Role { get; set; } = string.Empty;
+    public UserRole Role { get; set; }
 
     public override string ToString()
     {
