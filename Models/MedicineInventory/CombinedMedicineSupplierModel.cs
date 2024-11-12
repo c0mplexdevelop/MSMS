4de2cee149fabce0e89294b71b989d83a3393b56
c0mplex <snakebigbosslel@gmail.com>
2024-11-12 18:29:46 +0800

    User is now user duh

--- Models/MedicineInventory/CombinedMedicineSupplierModel.cs
+++ Models/MedicineInventory/CombinedMedicineSupplierModel.cs
@@ -6,6 +6,6 @@ public class CombinedMedicineSupplierModel
 {
     public IEnumerable<Medicine> Medicines { get; set; } = null!;
     public IEnumerable<Supplier> Suppliers { get; set; } = null!;
-    public IEnumerable<User> User { get; set; } = null!;
+    public User User { get; set; } = null!;
     public string SearchString { get; set; } = string.Empty;
 }
