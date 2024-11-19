a8564b68653381dcab6958af0a689c0aa6af5c89
c0mplex <snakebigbosslel@gmail.com>
2024-11-19 23:08:47 +0800

    Override equality operators

--- Models/Procedures/ActiveProcedure.cs
+++ Models/Procedures/ActiveProcedure.cs
@@ -17,4 +17,27 @@ public class ActiveProcedure
     public int ProcedureId { get; set; }
     public Procedure Procedure { get; set; } = null!;
     public DateTime ProcedureServiceDateTime { get; set; }
+
+    public static bool operator ==(ActiveProcedure left, ActiveProcedure right)
+    {
+        if(ReferenceEquals(left, right)) return true;
+        if (left is null || right is null) return false;
+        if(left.Id == right.Id && left.PatientId == right.PatientId && left.ProcedureId == right.ProcedureId && left.ProcedureServiceDateTime == right.ProcedureServiceDateTime)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    public static bool operator !=(ActiveProcedure left, ActiveProcedure right) => !(left == right);
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is ActiveProcedure activeProcedure)
+        {
+            return this == activeProcedure;
+        }
+        return false;
+    }
 }
